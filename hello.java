d74c06844894b479c762dd9f53f4aad9a98427e6
Nitansharma-gcet <nitanboi01@gmail.com>
2026-04-07 15:15:14 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
 public class hello {
   public static void main (String[] args){
-    System.out.println('Hello WORLD');
+    System.out.println('Hello WORLD!');
       }
 }
 
+
+// OUTPUT- Hello WORLD!
 
